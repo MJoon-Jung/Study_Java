2f3ac33f562ef03da7d1e0e2dbb973526999b978
MJoon-Jung <gjgjajaj31@gmail.com>
2021-01-03 19:47:48 +0900

    private

---
+++ src/oop/MyParentTest.java
@@ -0,0 +1,27 @@
+package oop;
+
+public class MyParentTest {
+
+	public static void main(String[] args) {
+		MyParent p = new MyParent();
+//		System.out.println(p.prv);
+		System.out.println(p.dft);
+		System.out.println(p.prt);
+		System.out.println(p.pub);
+		p.printMembers();
+	}
+
+}
+class MyParent{
+	private int prv;
+	int dft;
+	protected int prt;
+	public int pub;
+	
+	public void printMembers() {
+		System.out.println(prv);
+		System.out.println(dft);
+		System.out.println(prt);
+		System.out.println(pub);
+	}
+}
