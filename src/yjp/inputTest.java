4aff772f18764455e52cb5d5fd588106faf420a8
MJoon-Jung <gjgjajaj31@gmail.com>
2021-01-04 11:03:11 +0900

    String input

---
+++ src/yjp/inputTest.java
@@ -0,0 +1,29 @@
+package yjp;
+
+import java.util.Scanner;
+
+public class inputTest {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		while(true) {
+			System.out.println("Start input www~");
+			String s = sc.nextLine();
+			
+			if(s.equalsIgnoreCase("quit")) {
+				System.out.println("Exit");
+				break;
+			}
+			
+			if(s.startsWith("www")) {
+				System.out.println("www~ ");
+			}
+			else {
+				System.out.println("not www~");
+			}
+		}
+
+	}
+
+}
