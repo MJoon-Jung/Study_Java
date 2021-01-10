b3a25d1cee11832c0b761bb1d4492f2b24fa2c52
MJoon-Jung <gjgjajaj31@gmail.com>
2021-01-10 23:45:17 +0900

    Interface

---
+++ src/oop/InterfaceTest2.java
@@ -0,0 +1,29 @@
+package oop;
+
+public class InterfaceTest2 {
+
+	public static void main(String[] args) {
+		A a = new A();
+		a.autoPlay(new B());
+		a.autoPlay(new C());
+	}
+
+}
+class A{
+	void autoPlay(I i) {
+		i.play();
+	}
+}
+interface I{
+	public abstract void play();
+}
+class B implements I{
+	public void play() {
+		System.out.println("play in B");
+	}
+}
+class C implements I{
+	public void play() {
+		System.out.println("play in C");
+	}
+}
