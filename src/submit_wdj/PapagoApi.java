495cb31c7b844d21bfe78714aa3ccfec53712ae6
MJoon-Jung <gjgjajaj31@gmail.com>
2021-03-15 01:56:50 +0900

    submit

---
+++ src/submit_wdj/PapagoApi.java
@@ -0,0 +1,107 @@
+package submit_wdj;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+
+// 네이버 기계번역 (Papago SMT) API 예제
+public class PapagoApi {
+	private String txt;
+
+//	public static void main(String[] args) {
+//		PapagoApi papago = new PapagoApi();
+//		papago.setTxt("안녕하세요 재밌나요?");
+//		System.out.println(papago);
+//	}
+	public String toString() {
+        String clientId = "9AD54J5a2QVgWyge0KOr";//애플리케이션 클라이언트 아이디값";
+        String clientSecret = "iUk_DqTcPd";//애플리케이션 클라이언트 시크릿값";
+
+        String apiURL = "https://openapi.naver.com/v1/papago/n2mt";
+        String text;
+        try {
+            text = URLEncoder.encode(txt, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException("인코딩 실패", e);
+        }
+
+        Map<String, String> requestHeaders = new HashMap<>();
+        requestHeaders.put("X-Naver-Client-Id", clientId);
+        requestHeaders.put("X-Naver-Client-Secret", clientSecret);
+
+        String responseBody = post(apiURL, requestHeaders, text);
+
+        String [] ar = responseBody.split("\"translatedText\":");
+        String [] ans = ar[1].split(",");
+        ans[0] = ans[0].replace("\"", "");
+        
+        return ans[0];
+	}
+    private static String post(String apiUrl, Map<String, String> requestHeaders, String text){
+        HttpURLConnection con = connect(apiUrl);
+        String postParams = "source=ko&target=en&text=" + text; //원본언어: 한국어 (ko) -> 목적언어: 영어 (en)
+        try {
+            con.setRequestMethod("POST");
+            for(Map.Entry<String, String> header :requestHeaders.entrySet()) {
+                con.setRequestProperty(header.getKey(), header.getValue());
+            }
+
+            con.setDoOutput(true);
+            try (DataOutputStream wr = new DataOutputStream(con.getOutputStream())) {
+                wr.write(postParams.getBytes());
+                wr.flush();
+            }
+
+            int responseCode = con.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) { // 정상 응답
+                return readBody(con.getInputStream());
+            } else {  // 에러 응답
+                return readBody(con.getErrorStream());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("API 요청과 응답 실패", e);
+        } finally {
+            con.disconnect();
+        }
+    }
+
+    private static HttpURLConnection connect(String apiUrl){
+        try {
+            URL url = new URL(apiUrl);
+            return (HttpURLConnection)url.openConnection();
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("API URL이 잘못되었습니다. : " + apiUrl, e);
+        } catch (IOException e) {
+            throw new RuntimeException("연결이 실패했습니다. : " + apiUrl, e);
+        }
+    }
+
+    private static String readBody(InputStream body){
+        InputStreamReader streamReader = new InputStreamReader(body);
+
+        try (BufferedReader lineReader = new BufferedReader(streamReader)) {
+            StringBuilder responseBody = new StringBuilder();
+
+            String line;
+            while ((line = lineReader.readLine()) != null) {
+                responseBody.append(line);
+            }
+
+            return responseBody.toString();
+        } catch (IOException e) {
+            throw new RuntimeException("API 응답을 읽는데 실패했습니다.", e);
+        }
+    }
+	public void setTxt(String txt) {
+		this.txt = txt;
+	}
+}
--- src/submit_wdj/TextConverter.java
+++ src/submit_wdj/TextConverter.java
@@ -1,50 +1,76 @@
 package submit_wdj;
 
-import java.awt.GridBagLayout;
-import java.awt.GridLayout;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
-public class TextConverter extends JFrame implements ActionListener{
+public class TextConverter extends JFrame implements ActionListener, KeyListener{
 	private JButton convertBtn;
 	private JButton cancelBtn;
 	private JTextArea textIn;
 	private JTextArea textOut;
+	private PapagoApi papago = new PapagoApi();
 	
 	public TextConverter() {
+		JPanel panel = new JPanel();
+		panel.setBounds(0, 0, 786, 553);
+		this.getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("papago");
+		lblNewLabel.setFont(new Font("Arial", Font.PLAIN, 38));
+		lblNewLabel.setBounds(332, 10, 130, 46);
+		panel.add(lblNewLabel);
+		
 		textIn = new JTextArea(20,30);
+		textIn.setBounds(27,72,350,390);
+		textIn.setFont(new Font("±¼¸²", Font.PLAIN, 20));
+		textIn.addKeyListener(this);
+		
 		textOut = new JTextArea(20,30);
+		textOut.setBounds(406, 72, 350, 390);
+		textOut.setFont(new Font("Arial", Font.PLAIN, 20));
+		
 		textIn.setLineWrap(true);
 		textOut.setLineWrap(true);
 		textOut.setEditable(false);
 		
-		JPanel txtPanel = new JPanel();
 		
-		txtPanel.add(textIn);
-		txtPanel.add(textOut);
+		panel.add(textIn);
+		panel.add(textOut);
 		
 		convertBtn = new JButton("convert");
 		convertBtn.addActionListener(this);
+		convertBtn.setBounds(251, 472, 130, 51);
+		
 		cancelBtn = new JButton("cancel");
 		cancelBtn.addActionListener(this);
+		cancelBtn.setBounds(400,472,130,51);
 		
-		JPanel btnPanel = new JPanel();
-		btnPanel.add(convertBtn);
-		btnPanel.add(cancelBtn);
+		panel.add(convertBtn);
+		panel.add(cancelBtn);
 		
-		JPanel mainPanel = new JPanel(new GridLayout(2,1,20,20));
+//		JPanel btnPanel = new JPanel();
+//		btnPanel.add(convertBtn);
+//		btnPanel.add(cancelBtn);
+//		
+//		JPanel mainPanel = new JPanel(new GridLayout(2,1,20,20));
 		
-		mainPanel.add(txtPanel);
-		mainPanel.add(btnPanel);
+//		mainPanel.add(panel);
+//		mainPanel.add(btnPanel);
+//		this.add(mainPanel);
 		
-		this.add(mainPanel);
-		
-		this.setSize(700,500);
+		this.add(panel);
+		this.setSize(800,600);
 		this.setTitle("papago");
 		this.setDefaultCloseOperation(this.EXIT_ON_CLOSE);
 		this.setLocationRelativeTo(null);
@@ -55,8 +81,8 @@ public class TextConverter extends JFrame implements ActionListener{
 		if(e.getSource() == convertBtn) {
 			String s = textIn.getText();
 			String a = s.replace(" ", "");
+			a = a.replace("\n", "");
 			if(!a.equals("")) {				
-				ApiExamTranslateNmt papago = new ApiExamTranslateNmt();
 				papago.setTxt(s);
 				textOut.setText(papago.toString());
 			}
@@ -68,4 +94,19 @@ public class TextConverter extends JFrame implements ActionListener{
 	public static void main(String[] args) {
 		new TextConverter();
 	}
+	@Override
+	public void keyTyped(KeyEvent e) {
+		String s = textIn.getText();
+		String a = s.replace(" ", "");
+		a = a.replace("\n", "");
+		if(!a.equals("")) {
+			s.replace("\n", "<br>");
+			papago.setTxt(s);
+			textOut.setText(papago.toString());
+		}
+	}
+	@Override
+	public void keyPressed(KeyEvent e) {}
+	@Override
+	public void keyReleased(KeyEvent e) {}
 }
