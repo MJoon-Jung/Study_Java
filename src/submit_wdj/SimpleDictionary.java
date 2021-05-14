2584435898721e7e4dea9136388bbf26e6b2ab82
MJoon-Jung <gjgjajaj31@gmail.com>
2021-05-14 16:17:57 +0900

    add JDBC Connect

--- src/submit_wdj/SimpleDictionary.java
+++ src/submit_wdj/SimpleDictionary.java
@@ -7,6 +7,10 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
@@ -38,7 +42,115 @@ public class SimpleDictionary extends JPanel implements ActionListener{
 		this.add(btn_add);
 		
 		this.setPreferredSize(new Dimension(450,70));
-		buildDictionaryFromFile();
+//		buildDictionaryFromFile();
+		buildDictionaryFromDB();
+	}
+
+	private void buildDictionaryFromDB() {
+
+		
+	}
+	private void addWordToDB(String key, String value){
+		Connection con = null;
+		PreparedStatement ps = null;
+		try {			
+			con = DBCon.getConnection();
+			ps = con.prepareStatement("INSERT INTO dict VALUES(?,?)");
+			ps.setString(1, key);
+			ps.setString(2, value);
+			ps.executeUpdate();
+			
+			System.out.println("¼º°ø");
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+//		finally {
+//		try {
+//			if(ps != null) {
+//				ps.close();
+//			}
+//			if(con != null) {
+//				con.close();
+//			}
+//		}catch(SQLException e) {
+//			e.printStackTrace();
+//		}
+//	}
+	}
+	private String selectWord1ToDB(String key) {
+		String ans = null;
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		try {
+			con = DBCon.getConnection();
+			ps = con.prepareStatement("SELECT word2 FROM dict WHERE word1=?");
+			ps.setString(1, key);
+			rs = ps.executeQuery();
+			
+			while(rs.next()) {
+				String word2 = rs.getString("word2");
+				ans = word2;
+				System.out.println(word2);
+			}
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+//		finally {
+//		try {
+//			if(rs != null) {
+//				rs.close();
+//			}
+//			if(ps != null) {
+//				ps.close();
+//			}
+//			if(con != null) {
+//				con.close();
+//			}
+//		}catch(SQLException e) {
+//			e.printStackTrace();
+//		}
+//	}
+		return ans;
+	}
+	private String selectWord2ToDB(String value) {
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		String ans=null;
+		try {
+			con = DBCon.getConnection();
+			ps = con.prepareStatement("SELECT word1 FROM dict WHERE word2=?");
+			ps.setString(1, value);
+			rs = ps.executeQuery();
+			
+			while(rs.next()) {
+				String word1 = rs.getString("word1");
+				ans = word1;
+				System.out.println(word1);
+			}
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+//		finally {
+//			try {
+//				if(rs != null) {
+//					rs.close();
+//				}
+//				if(ps != null) {
+//					ps.close();
+//				}
+//				if(con != null) {
+//					con.close();
+//				}
+//			}catch(SQLException e) {
+//				e.printStackTrace();
+//			}
+//		}
+		return ans;
 	}
 
 	private void buildDictionaryFromFile() {
@@ -66,7 +178,13 @@ public class SimpleDictionary extends JPanel implements ActionListener{
 		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 	}
-
+	private void abc(String key,String value) {
+		if(value == null) {
+			JOptionPane.showMessageDialog(this, "not found", key,JOptionPane.ERROR_MESSAGE);
+		}else {
+			JOptionPane.showMessageDialog(this, value, key, JOptionPane.INFORMATION_MESSAGE);
+		}
+	}
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		String key = inputField.getText().trim();
@@ -76,27 +194,56 @@ public class SimpleDictionary extends JPanel implements ActionListener{
 			return;
 		}
 		if(e.getSource() == btn_search) {
-			String value = dict.get(key);
-			if(value == null) {
-				char c = key.charAt(0);
-				if((c >= 65 && c <= 90) || (c >=97 && c<= 122)) {
-					String ans = getKey(key);
-					JOptionPane.showMessageDialog(this, ans, key, JOptionPane.INFORMATION_MESSAGE);
-				}else {					
-					JOptionPane.showMessageDialog(this, "not found", key,JOptionPane.ERROR_MESSAGE);
-				}
-			}else {			
-				JOptionPane.showMessageDialog(this, value, key, JOptionPane.INFORMATION_MESSAGE);
+			char c = key.charAt(0);
+			if((c >= 65 && c <= 90) || (c >= 97 && c <= 122)) {
+				String value = selectWord2ToDB(key);
+				abc(key,value);
+			}else {
+				String value = selectWord1ToDB(key);
+				abc(key,value);
 			}
 		}else {
 			String en = JOptionPane.showInputDialog(this, "input English").trim();
 			if(en == null || en.length() == 0) return;
-			dict.put(key, en);
-			addWordToFile(key, en);
+//			dict.put(key, en);
+//			addWordToFile(key, en);
+			addWordToDB(key, en);
 			JOptionPane.showMessageDialog(this, "word save");
 		}
 		inputField.setText("");
 	}
+//	@Override
+//	public void actionPerformed(ActionEvent e) {
+//		String key = inputField.getText().trim();
+//		
+//		if(key.length() == 0) {
+//			inputField.setText("");
+//			return;
+//		}
+//		if(e.getSource() == btn_search) {
+//			
+//			String value = dict.get(key);
+//			if(value == null) {
+//				char c = key.charAt(0);
+//				if((c >= 65 && c <= 90) || (c >=97 && c<= 122)) {
+//					String ans = getKey(key);
+//					JOptionPane.showMessageDialog(this, ans, key, JOptionPane.INFORMATION_MESSAGE);
+//				}else {					
+//					JOptionPane.showMessageDialog(this, "not found", key,JOptionPane.ERROR_MESSAGE);
+//				}
+//			}else {			
+//				JOptionPane.showMessageDialog(this, value, key, JOptionPane.INFORMATION_MESSAGE);
+//			}
+//		}else {
+//			String en = JOptionPane.showInputDialog(this, "input English").trim();
+//			if(en == null || en.length() == 0) return;
+//			dict.put(key, en);
+////			addWordToFile(key, en);
+//			addWordToDB(key, en);
+//			JOptionPane.showMessageDialog(this, "word save");
+//		}
+//		inputField.setText("");
+//	}
 	private String getKey(String value) {
         for (String key : dict.keySet()) {
             if (value.equals(dict.get(key))) {
